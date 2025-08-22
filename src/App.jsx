4f17829a43e26e1d449c5e507318b7085a95c1d2
marroccuandrea <marroccuandrea@yahoo.it>
2025-08-22 14:05:38 +0200

    [add] Implement search component and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
       <main>
-        <div className="header_wrapper">
+        <div className="pattern"></div>
+        <div className="header_wrapper wrapper">
           <header>
-            <h1>Trova i Film Che Ti Piacciono Senza Problemi</h1>
+            <img src="./img/hero-img.png" alt="Hero Banner" />
+            <h1>
+              Trova i <span className="text-gradient">Film</span> Che Ti
+              Piacciono Senza Problemi
+            </h1>
           </header>
+          {/* Sezione ricerca */}
+          <Search />
         </div>
       </main>
     </>
